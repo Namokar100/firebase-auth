2530bfd2b1b9bfdf3537989578ec8047df87019f
Namokar100 <namokarsvg@gmail.com>
2025-04-12 23:39:34 +0530

    Created a middleware to check auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,6 @@
-import { isAuthenticated } from '@/lib/actions/auth.action';
-import { redirect } from 'next/navigation';
 import React from 'react'
 
 const AuthLayout = async ({children}: {children: React.ReactNode}) => {
-    const isUserAuthenticated = await isAuthenticated();
-
-    if(isUserAuthenticated){
-      redirect("/home");
-    }
   return (
     <div>
         {children}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,6 @@
-import { isAuthenticated } from '@/lib/actions/auth.action';
-import { redirect } from 'next/navigation';
 import React from 'react'
 
 const RootLayout = async ({children}: {children: React.ReactNode}) => {
-    // const isUserAuthenticated = await isAuthenticated();
-
-    // if(!isUserAuthenticated){
-    //   redirect("/sign-in");
-    // }
   return (
     <div>
        {children}
